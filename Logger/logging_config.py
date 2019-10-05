52ca46a45c04d4e670e1a38b794675da9c248f20
sdmunozsierra <sdmunozsierra@miners.utep.edu>
2019-10-05 16:51:25 +0000

    Added custom formatters and reST docstrings.

--- Logger/logging_config.py
+++ Logger/logging_config.py
@@ -5,14 +5,15 @@ from module_config import ROOT_DIR
 
 # Create a simple logger in default directory "/Logger"
 def get_simple_logger(log_name, log_level=logging.DEBUG):
-    """Create a simple logger in the default directory."""
+    """Create a simple logger in the default directory.
+    :param log_name: Name of the logger and logger_name.log.
+    """
     # Create Logger with name and level
     logger = logging.getLogger(log_name)
     logger.setLevel(log_level)
 
-    # Define a formatter for the Logger
-    formatter = logging.Formatter(
-        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    # Define a formatter for the Logger (Defaults to simple)
+    formatter = set_formatter_format('simple')
 
     # Set path of the logger using ROOT_DIR and log_name
     path = f"{ROOT_DIR}/Logger/{log_name}.log"
@@ -24,3 +25,32 @@ def get_simple_logger(log_name, log_level=logging.DEBUG):
     logger.addHandler(fileh)
 
     return logger  # Return Logger
+
+
+def set_formatter_format(option='simple'):
+    """Choose a formatter from the following or create your own.
+    `simple` - Time LoggerName Level - Message
+    `process` - Time moduleName -> processId lineNo Level - Message
+    `function` - Time moduleName funcName -> lineNo Level - Message
+    :param options: simple, process, function, custom.
+    :return: formatter to be set with `setFormatter`.
+    """
+    # Check for valid input
+    options = ['simple', 'process', 'function']
+    if option not in options:
+        print("Input error!")
+        raise ValueError
+    # Define a simple formatter
+    if option == 'simple':
+        return logging.Formatter(
+            '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    # Define a process formatter
+    if option == 'process':
+        return logging.Formatter(
+            '%(asctime)s %(module)s -> %(process)d %(lineno)d %(levelname)s' +
+            ' -  %(message)s')
+    # Define a function formatter
+    if option == 'process':
+        return logging.Formatter(
+            '%(asctime)s %(module)s %(funcName)s -> %(lineno)d %(levelname)s' +
+            ' -  %(message)s')
